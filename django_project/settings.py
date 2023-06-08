631d0568ef9711ba5ae14d1a89aeb9bb46e4b1a6
Yelnur <ent.elnur.tur@gmail.com>
2023-06-08 11:09:07 +0600

    created base.css

--- django_project/settings.py
+++ django_project/settings.py
@@ -131,3 +131,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'home' # registration жасау үшін
 LOGOUT_REDIRECT_URL = 'home' # аккаунттан шығу үшін қажет
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
